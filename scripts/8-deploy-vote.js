c8642c99769137f0e17971a5848e0a1fb8e8f0d6
Flint <14si2o@proton.me>
2022-11-20 21:03:06 +0100

    added airdrop token

---
+++ scripts/8-deploy-vote.js
@@ -0,0 +1,17 @@
+import sdk from './1-initialize-sdk.js';
+
+
+(async () =>{
+    try {
+        const voteContractAddress = await sdk.deployer.deployVote({
+            name: "Dwarf DAO",
+            voting_token_address: "0x8f005938701D96778B7d9E2d3dBE90E6DCfACae8",
+            voting_delay_in_blocks: 0,
+            voting_period_in_blocks: 6570,
+            voting_quorum_fraction: 0,
+            proposal_token_threshold: 0,
+        });
+    } catch (error) {
+        
+    }
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useAddress, ConnectWallet, Web3Button, useContract, useNFTBalance } from '@thirdweb-dev/react';
+import { checkProperties } from 'ethers/lib/utils';
 import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
@@ -8,12 +9,67 @@ const App = () => {
   const editionDropAddress = "0x937dB0C3B07D3C87dD7bEC0146F2c5866b8Cc05c";
   // Initialize our Edition Drop contract
   const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  const { contract: token } = useContract("0x8f005938701D96778B7d9E2d3dBE90E6DCfACae8", "token");
   // Hook to check if the user has our NFT
   const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
 
   const hasClaimedNFT = useMemo(() => {
     return nftBalance && nftBalance.gt(0)
   }, [nftBalance])
+
+
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const shortenAddress = (str) => {
+    return str.substring(0,6)+'...'+ str.substring(str.length - 4 );
+  }
+
+  useEffect( () => {
+    if(!hasClaimedNFT){
+      return;
+    }
+    const getAllAddresses = async () => {
+    try {
+      const memberAddresses = await editionDrop?.history.getAllClaimerAddresses(0,);
+      setMemberAddresses(memberAddresses);
+      console.log ('Members addresses', memberAddresses);      
+    } catch (error) {
+      console.error("failed to get member list", error);      
+    }
+    };
+    getAllAddresses();
+  }, [hasClaimedNFT, editionDrop?.history]);
+
+
+  useEffect( () => {
+    if(!hasClaimedNFT){
+      return;
+    }
+    const getAllBalances = async () => {
+      try {
+        const amounts = await token?.history.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log ('Amounts', amounts);
+      } catch (error) {
+        console.error('failed to get member balances', error);
+      }
+    };
+    getAllBalances();
+  },[hasClaimedNFT,token?.history]);
+
+  const memberList= useMemo(() => {
+    return memberAddresses.map((address) => {
+      const member = memberTokenAmounts?.find(({holder}) => holder === address);
+
+      return {
+        address,
+        tokenAmount: member?.balance.displayValue || '0',
+      };
+    });
+  },[memberAddresses,memberTokenAmounts]);
+
+
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
@@ -28,14 +84,39 @@ const App = () => {
     );
   }
 
-  if(hasClaimedNFT){
-    return(
+  if (hasClaimedNFT) {
+    return (
       <div className="member-page">
-        <h1>DF DOA Member Page</h1>
-        <p>congratulations on being a Dwarf</p>
+        <h1>Dwarf Fortress DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
+
+
 
   // Render mint nft screen.
   return (
